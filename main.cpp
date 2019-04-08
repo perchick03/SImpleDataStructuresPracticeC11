ee71ecd966a39c86df7611e8872201c306ef90f8
Peretzl Levinov <perchick03@gmail.com>
2019-04-08 05:21:02 +0300

    Simple Binary Search Tree

--- main.cpp
+++ main.cpp
@@ -2,16 +2,28 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-//#include "linked_list.h"
-
-
-//using namespace exercise;
-//using namespace std;
-
+#include "SimpleBinaryTree.h"
 
 int main(int argc, char* argv[]) {
+    using namespace exercise;
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
 
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    SimpleBinaryTree<int> tree;
+    tree.InsertNode(100);
+    tree.InsertNode(200);
+    tree.InsertNode(300);
+    //tree.InsertNode(400);
+    tree.InsertNode(150);
+    tree.InsertNode(20);
+    tree.InsertNode(200);
+    tree.InsertNode(2);
+    tree.InsertNode(0);
+    tree.InsertNode(3);
+    tree.InsertNode(10);
 
+    std::cout<<tree<<std::endl;
+    std::cout<<"height: "<<tree.height()<<std::endl;
+    //tree.PrintTree();
+    return 0;
 }
