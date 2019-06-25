6a1b594a2130456a2a6b3adcf64e961981c24a9b
Peretzl Levinov <perchick03@gmail.com>
2019-06-25 19:23:00 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/Exercis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercis.dir/main.cpp.obj"
+  "CMakeFiles/Exercis.dir/Tests/TestGtest.cpp.obj"
+  "Exercis.pdb"
+  "Exercis.exe"
+  "Exercis.exe.manifest"
+  "libExercis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
