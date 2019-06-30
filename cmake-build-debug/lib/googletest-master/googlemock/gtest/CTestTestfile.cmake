84834ba7d5e23c2a8a64640a4c06c49194afbc95
Peretzl Levinov <perchick03@gmail.com>
2019-06-30 17:49:46 +0300

    adding .gitignore

--- cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/CLionProjects/Exercis/lib/googletest-master/googletest
-# Build directory: C:/Users/User/CLionProjects/Exercis/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
